40d4deb742ab099ab6359737bb00e7817506a944
liardanc3 <liardanc3@gmail.com>
2021-08-23 18:02:42 +0900

    boj

--- 6064.cpp
+++ 6064.cpp
@@ -1,29 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	int T, M, N, X, Y, k, ans;
-	cin >> T;
-	for (int i = 0; i < T; i++) {
+	int T;	cin >> T;
+	while(T--) {
+		int M, N, X, Y, ans = -1;
 		cin >> M >> N >> X >> Y;
-		k = -1;
-		if (X > Y) {
-			int tmp = Y;
-			Y = X;
-			X = tmp;
-			tmp = N;
-			N = M;
-			M = tmp;
-		}
-		for (int i = 0; i < N; i++) {
-			if ((M * i) % N == Y - X) {
-				k = i; break;
-			}
-		}
-		k==-1? ans = -1 : ans=M*k+X;
-		cout << ans << '\n';
+		if(X > Y)  { swap(X, Y); swap(M, N); }
+		for (int i = 0; i < N && ans==-1; i++) 
+			(M * i) % N == Y - X ? ans = i : 0;
+		ans == -1 ? printf("-1\n") : printf("%d\n",ans*M+X);
 	}
-	return 0;
 }
