76f44bc4e32dcef49047debb4f27b2dc60c9d410
liardanc3 <liardanc3@gmail.com>
2021-09-02 11:41:39 +0900

    boj

--- 11660.cpp
+++ 11660.cpp
@@ -1,28 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arr[1025][1025]{ 0, }, dp[1025][1025]{ 0, }, n, m, tmp;
-int dynamic(int x1, int y1, int x2, int y2) {
-	int ans = 0;
-	for (int i = x1; i <= x2; i++) {
-		ans += dp[i][y2] - dp[i][y1 - 1];
-	}
-	return ans;
-}
+int arr[1025][1025]{ 0, }, dp[1025][1025]{ 0, }, n, m;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	cin >> n >> m;
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
-			cin >> tmp;
-			arr[i][j] = tmp;
-			dp[i][j] = dp[i][j - 1] + arr[i][j];
+			cin >> arr[i][j];
+			dp[i][j] = dp[i][j - 1] + dp[i-1][j] - dp[i-1][j-1] + arr[i][j];
 		}
 	}
 	int x1, x2, y1, y2;
 	while (m--) {
 		cin >> x1 >> y1 >> x2 >> y2;
-		cout << dynamic(x1,y1,x2,y2) << '\n';
+		cout << dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] << '\n';
 	}
 	return 0;
 }
