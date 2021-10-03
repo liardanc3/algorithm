5363ce4062861740ae05617bd17704797390408e
liardanc3 <liardanc3@gmail.com>
2021-10-03 20:45:02 +0900

    boj

--- tmp.cpp
+++ tmp.cpp
@@ -1,66 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, tmp, now;
-vector<int> inorder;
-vector<int> postorder;
-int tree[100001];
+int N, M, K;
+typedef pair<int, int> pii;
+vector<int> vect[100001];
+int ck[100001];
+vector<pii> fr;
+queue<int> QQQ;
+int fun() {
+	int cutcount = 0;
+	while (!fr.empty()) {
+		memset(ck, 0, sizeof(ck));
+		int check = 1;
+		int flag = 1;
+		queue<int> q;
+		q.push(1);
+		ck[1] = 1;
+		while (!q.empty()) {
+			int size = q.size();
 
-void make_tree(int root_idx, int in_L, int in_R, int post_L, int post_R) {
-	if (in_L >= in_R || post_L >= post_R) return;
-	
-	int left_range = root_idx - in_L;
-	if (left_range != 0) {
-		int left_root = postorder.at(post_L - 1 + left_range);
-		int left_root_idx;
-		for (int i = in_L; i < root_idx; i++) {
-			if (inorder.at(i) == left_root) {
-				left_root_idx = i;
-				break;
+			flag = -flag;
+			for (int j = 0; j < size; j++) {
+				int now = q.front();
+				q.pop();
+				
+				for (int i = 0; i < vect[now].size(); i++) {
+					int next = vect[now].at(i);
+					if (next == -1) continue;
+
+					if (ck[next] == 0) {
+						ck[next] = flag;
+						q.push(next);
+					}
+					else if (ck[next] == flag)
+						continue;
+					else {
+						check = 0;
+						while (!q.empty())
+							q.pop();
+						j = size;
+						break;
+					}
+				}
 			}
 		}
-		tree[now++] = left_root;
-		make_tree(left_root_idx, in_L, root_idx - 1, post_L, post_L + left_range - 1);
+		if (check == 0) {
+			if (QQQ.empty()) return -1;
+			pii now = fr.at(QQQ.front()-1);
+			QQQ.pop();
+			for (int i = 0; i < vect[now.first].size(); i++) {
+				if (vect[now.first].at(i) == now.second)
+					vect[now.first].at(i) = -1;
+			}
+			cutcount++;
+		}
+		else {
+			return cutcount;
+		}
 	}
 	
-	int right_range = in_R - root_idx;
-	if (right_range != 0) {
-		int right_root = postorder.at(post_R - 1);
-		int right_root_idx;
-		for (int i = root_idx + 1; i <= in_R; i++) {
-			if (inorder.at(i) == right_root) {
-				right_root_idx = i;
-				break;
-			}
-		}
-		tree[now++] = right_root;
-		make_tree(right_root_idx, root_idx + 1, in_R, post_L + left_range, post_R - 1);
-	}
 }
 
 int main() {
-	cin >> n;
-	inorder.push_back(-1);
-	postorder.push_back(-1);
-	for (int i = 1; i <= n; i++) {
-		cin >> tmp;
-		inorder.push_back(tmp);
+	cin >> N >> M >> K;
+	int a, b;
+	for (int i = 1; i <= M; i++) {
+		cin >> a >> b;
+		vect[a].push_back(b);
+		fr.push_back(pii(a, b));
 	}
-	for (int i = 1; i <= n; i++) {
-		cin >> tmp;
-		postorder.push_back(tmp);
+	for (int i = 1; i <= K; i++) {
+		cin >> a;
+		QQQ.push(a);
 	}
-	int root = postorder.at(postorder.size() - 1);
-	int root_idx;
-	for (int i = 1; i <= n; i++) {
-		if (inorder.at(i) == root) {
-			root_idx = i;
-			break;
-		}
+	int ans = fun();
+	cout << ans;
+	if (ans == -1) return 0;
+	int a1=0, b1=0;
+	for (int i = 1; i <= N; i++) {
+		if (ck[i] == 1)
+			a1++;
+		else b1++;
 	}
-	now = 1;
-	tree[now++] = root;
-	make_tree(root_idx, 1, n, 1, n);
-	for (int i = 1; i <= n; i++)
-		cout << tree[i] << ' ';
-	return 0;
+	if (a1 > b1) swap(a1, b1);
+	cout << '\n' << a1 << ' ' << b1;
 }
