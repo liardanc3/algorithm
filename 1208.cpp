cefef2d85e3e7b4be4614c6f261aebb9d9e65a64
liardanc3 <liardanc3@gmail.com>
2021-10-30 01:31:03 +0900

    boj

--- 1208.cpp
+++ 1208.cpp
@@ -1,68 +1,43 @@
 #include <bits/stdc++.h>
+#define zero 2000000 
 using namespace std;
-int N, S, cnt;
-typedef long long ll;
-ll tmp;
-vector<ll> vect;
-vector<int> m1, m2;
-
+int N, S, tmp;
+long long ans = 0;
+vector<int> vect1, vect2;
+int _m[8000010];
+int *m = &_m[4000005];
+void combination1(int idx, int sum) {
+	if (idx == N/2) {
+		m[sum]++;
+		return;
+	}
+	combination1(idx + 1, sum);
+	combination1(idx + 1, sum + vect1[idx]);
+}
+void combination2(int idx, int sum) {
+	if (idx == N - N/2) {
+		ans += m[S-sum];
+		return;
+	}
+	combination2(idx + 1, sum);
+	combination2(idx + 1, sum + vect2[idx]);
+}
 int main() {
+	memset(_m, 0, sizeof(_m));
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	m1.push_back(0); m2.push_back(0);
-	cnt = 0;
 	cin >> N >> S;
-	for (int i = 0; i < N; i++) {
+	for (int i = 1; i <= N / 2; i++) {
 		cin >> tmp;
-		vect.push_back(tmp);
+		vect1.push_back(tmp);
 	}
-	//sort(vect.begin(), vect.end());
-	int halfL = N / 2, halfR = N - halfL;
-	for (int i = 1; i < pow(2, halfL); i++) {
-		int sum = 0, count = 0;
-		bitset<20> bit(i);
-		for (int k = 0; count < bit.count(); k++) {
-			if (bit[k] == 1) {
-				count++;
-				sum += vect[k];
-			}
-		}
-		m1.push_back(sum);
-	} sort(m1.begin(), m1.end());
-	for (int i = 1; i < pow(2, halfR); i++) {
-		int sum = 0, count = 0;
-		bitset<20> bit(i);
-		for (int k = 0; count < bit.count(); k++) {
-			if (bit[k] == 1) {
-				count++;
-				sum += vect[k+halfL];
-			}
-		}
-		m2.push_back(sum);
-	} sort(m2.begin(), m2.end());
-
-	ll ans = 0;
-	int left = 0, right = m2.size() - 1;
-	while (left < m1.size() && right >= 0) {
-		if (m1[left] + m2[right] == S) {
-			ll L = 1, R = 1;
-
-			while (left + 1 < m1.size() && m1[left] == m1[left + 1]) {
-				left++;
-				L++;
-			} left++;
-			while (right - 1 >= 0 && m2[right] == m2[right - 1]) {
-				right--;
-				R++;
-			} right--;
-
-			ans += (L * R);
-		}
-		else if (m1[left] + m2[right] < S)
-			left++;
-		else right--;
+	for (int i = 1; i <= N - N / 2; i++) {
+		cin >> tmp;
+		vect2.push_back(tmp);
 	}
-	if (S == 0) ans--;
+	combination1(0, 0);
+	combination2(0, 0);
+	if (!S) ans--;
 	cout << ans;
 	return 0;
 }
