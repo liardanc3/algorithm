b6d35eea53b600bb94ed01e2dbb55f3b31f012bc
liardanc3 <liardanc3@gmail.com>
2021-11-03 12:36:20 +0900

    boj

--- 3860.cpp
+++ 3860.cpp
@@ -3,97 +3,78 @@ using namespace std;
 #define INF 1e9
 typedef pair<int, int> pii;
 typedef tuple<int, int, int> tii;
-int W, H, G, Y, X, E, Y1, X1, Y2, X2, T;
-int arr[31][31];
-int d[31][31];
-int ck[31][31];
-int dy[]{ -1,0,0,1 };
-int dx[]{ 0,-1,1,0 };
-map<pii, tii> M;
-
-void bfs() {
-	priority_queue<tii,vector<tii>,greater<tii>> q;
-	q.push(tii(d[1][1],1, 1));
-	while (!q.empty()) {
-		int dist = get<0>(q.top());
-		int y = get<1>(q.top());
-		int x = get<2>(q.top());
-		q.pop();
-
-		// ±Í½Å±¸¸ÛÀÎ °æ¿ì
-		if (arr[y][x] == 1) {
-			tii hole = M[pii(y, x)];
-			int ddy = get<0>(hole);
-			int ddx = get<1>(hole);
-			int dis = get<2>(hole);
-
-			if (dis + dist < d[ddy][ddx]) {
+int W, H, G, X, Y, E, X1, Y1, X2, Y2, T, d[901], arr[30][30];
+int dy[]{ -1,0,0,1 }, dx[]{ 0,-1,1,0 };
+vector<pii> vect[901];
+void fun() {
+	int N = W * H;
+	for (int i = 0; i < W; i++) {
+		for (int j = 0; j < H; j++) {
+			int now = arr[i][j];
+			int now_n = j * W + i;
+			if (now == -1 || now == -2 || now_n == N-1) continue;
 			
-				ck[y][x]++;
-				d[ddy][ddx] = dist + dis;
-				q.push(tii(d[ddy][ddx], ddy, ddx));
-				
-			}
-			if (d[1][1] < 0) {
-				cout << "Never\n";
-				return;
-			}
-		}
-
-		// ±Í½Å±¸¸ÛÀÌ ¾Æ´Ñ°æ¿ì
-		else {
-			for (int i = 0; i < 4; i++) {
-				int ddy = y + dy[i];
-				int ddx = x + dx[i];
-				if (1 <= ddy && ddy <= W && 1 <= ddx && ddx <= H) {
-
-					if (ddy == W && ddx == H) {
-						d[ddy][ddx] = min(d[ddy][ddx],dist + 1);
-						//d[ddy][ddx] = dist + 1;
-						continue;
-					}
-
-					if (arr[ddy][ddx] == 0 || arr[ddy][ddx]==1) {
-						if (d[y][x] + 1 < d[ddy][ddx]) {
-							d[ddy][ddx] = dist + 1;
-							q.push(tii(d[ddy][ddx], ddy, ddx));
-						}
-					}
-
-				}
-				if (d[1][1] < 0) {
-					cout << "Never\n";
-					return;
+			for (int k = 0; k < 4; k++) {
+				if (0 <= i + dx[k] && i + dx[k] < W && 0 <= j + dy[k] && j + dy[k] < H) {
+					int next = arr[i + dx[k]][j + dy[k]];
+					int next_n = (j + dy[k]) * W + i + dx[k];
+					if (next != -1)
+						vect[now_n].push_back({ next_n,1 });
 				}
 			}
 		}
-		
 	}
-	if (d[W][H] == INF) cout << "Impossible\n";
-	else cout << d[W][H] << '\n';
-	return;
+
+	// bellmanford
+	
+	d[0] = 0;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (d[j] == INF) continue;
+			for (int k = 0; k < vect[j].size(); k++) {
+				int next = vect[j][k].first;
+				int dist = vect[j][k].second;
+
+				if (d[next] > d[j] + dist) {
+					d[next] = d[j] + dist;
+
+					if (i == N) {
+						d[N - 1] = -INF;
+						//cout << i << " d[N-1] = -INF\n";
+					}
+				}
+			}
+		}
+	}
+	if (d[N - 1] == -INF) cout << "Never\n";
+	else if (d[N - 1] == INF) cout << "Impossible\n";
+	else cout << d[N - 1] << '\n';
 }
+
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 	while (cin >> W >> H) {
-		memset(ck, 0, sizeof(ck));
+		fill(&d[0], &d[901], INF);
+		for (int i = 0; i <= 900; i++)
+			vect[i].clear();
+		vect->clear();
 		memset(arr, 0, sizeof(arr));
-		M.clear();
-		fill(&d[0][0], &d[30][31], INF);
-		d[1][1] = 0;
 		if (W == 0 && H == 0) break;
 		cin >> G;
-		while (G--) {
-			cin >> Y >> X;
-			arr[Y + 1][X + 1] = -1;
+		for (int i = 0; i < G; i++) {
+			cin >> X >> Y;
+			arr[X][Y] = -1;
 		}
 		cin >> E;
-		while (E--) {
-			cin >> Y1 >> X1 >> Y2 >> X2 >> T;
-			arr[Y1 + 1][X1 + 1] = 1;
-			M.insert(pair<pii, tii>(pii(Y1 + 1, X1 + 1), tii(Y2 + 1, X2 + 1, T)));
+		for (int i = 0; i < E; i++) {
+			cin >> X1 >> Y1 >> X2 >> Y2 >> T;
+			arr[X1][Y1] = -2;
+			int now = Y1 * W + X1;
+			int next = Y2 * W + X2;
+			vect[now].push_back({ next,T });
 		}
-		bfs();
+		fun();
 	}
-
 	return 0;
 }
