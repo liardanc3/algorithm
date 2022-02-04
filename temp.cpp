f5aca1dd689fd1d7a50defb29fbb51de1138aada
liardanc3 <liardanc3@gmail.com>
2022-02-05 02:05:42 +0900

    boj

--- temp.cpp
+++ temp.cpp
@@ -1,31 +1,34 @@
 #include <bits/stdc++.h>
-#define int long long
-#define DIV 1'000'000'007
 using namespace std;
-typedef tuple<int, int, int, int> matrix;
-int n;
-matrix MUL(matrix m1, matrix m2) {
-	int a, b, c, d;
-	a = get<0>(m1) * get<0>(m2) + get<1>(m1) * get<2>(m2);
-	b = get<0>(m1) * get<1>(m2) + get<1>(m1) * get<3>(m2);
-	c = get<2>(m1) * get<0>(m2) + get<3>(m1) * get<2>(m2);
-	d = get<2>(m1) * get<1>(m2) + get<3>(m1) * get<3>(m2);
-	return { a%DIV ,b%DIV ,c%DIV ,d%DIV };
-}
-matrix POW(int i) {
-	if (i == 1)
-		return { 0,1,1,1 };
-	matrix half = POW(i / 2);
-	half = MUL(half, half);
-	if (i % 2) half = MUL(half, { 0,1,1,1 });
-	return half;
-}
+int n, dp[300001];
+vector<int> ps;
 signed main() {
-	cin >> n;
-	if (n == 0) cout << 0;
-	else {
-		matrix m = POW(n);
-		cout << get<1>(m);
+	dp[1] = 1, dp[4] = 1;
+	ps.push_back(1), ps.push_back(4);
+	for (int i = 3;; i++) {
+		int now = i * i + ps[i - 3];
+		if (now > 300000) break;
+		ps.push_back(now);
+		dp[now] = 1;
 	}
+	for (int i = 1; i <= 300000; i++) {
+		if (!dp[i]) dp[i] = i;
+	}
+	for (int i = 0; i < ps.size() - 1; i++) {
+		for (int j = i + 1; j < ps.size(); j++) {
+			int now = ps[i];
+			int next = ps[j];
+			for (int k = now + next, cnt = 2; k <= 300000; k += next, cnt++)
+				dp[k] = min(dp[k], cnt);
+		}
+	}
+	cin >> n;
+	for (int i = 1; i <= n - 1; i++) {
+		int left = dp[n - i];
+		int right = dp[i];
+		if (left == 1e9 || right == 1e9) continue;
+		else dp[n] = min(dp[n], left + right);
+	}
+	cout << dp[n];
 	return 0;
 }
