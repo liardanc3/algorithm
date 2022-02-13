c4ac96008846d5e6cd44990fdceddb3344da3c70
liardanc3 <liardanc3@gmail.com>
2022-02-14 00:29:48 +0900

    boj

--- tmp.cpp
+++ tmp.cpp
@@ -1,19 +1,25 @@
-#include <bits/stdc++.h>
-#define FASTIO ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
-#define int long long
-#define PI 3.141592
-#define PARAM 20037508.34
+﻿#include <iostream>
 
 using namespace std;
 
-double lon, lat, x, y;
+void main() {
 
-signed main() {
-	cin >> lon >> lat;
-	lon = 126.6328428, lat = 37.378413;
-	x = (lon * PARAM) / 180;
-	y = log(tan(((90 + lat) * PI) / 360)) / (PI / 180);
-	y = (y * PARAM) / 180;
-	cout << x << ' ' << y;
-	return 0;
+    double e_value = 2.7182818284;
+    double X = 20037508.34;
+
+    double lat3857, long3857;
+
+    cin >> lat3857 >> long3857;
+    //converting the longitute from epsg 3857 to 4326
+    double long4326 = (long3857 * 180) / X;
+
+    //converting the latitude from epsg 3857 to 4326 split in multiple lines for readability        
+    double lat4326 = lat3857 / (X / 180);
+    double exponent = (3.141592 / 180) * lat4326;
+
+    lat4326 = atan(pow(e_value, exponent));
+    lat4326 = lat4326 / (3.141592 / 360); // Here is the fixed line
+    lat4326 = lat4326 - 90;
+    cout << lat4326 << ' ' << long4326;
+    exit(0);
 }
