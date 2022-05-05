8df0dc7162cc373f8945495a630431722cb71f66
liardanc3 <liardanc3@gmail.com>
2022-05-06 02:49:03 +0900

    boj

--- temp.cpp
+++ temp.cpp
@@ -1,34 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, dp[300001];
-vector<int> ps;
-signed main() {
-	dp[1] = 1, dp[4] = 1;
-	ps.push_back(1), ps.push_back(4);
-	for (int i = 3;; i++) {
-		int now = i * i + ps[i - 3];
-		if (now > 300000) break;
-		ps.push_back(now);
-		dp[now] = 1;
+string s;
+vector<string> vect;
+int main() {
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		vect.push_back(s.substr(i, s.size()));
 	}
-	for (int i = 1; i <= 300000; i++) {
-		if (!dp[i]) dp[i] = i;
-	}
-	for (int i = 0; i < ps.size() - 1; i++) {
-		for (int j = i + 1; j < ps.size(); j++) {
-			int now = ps[i];
-			int next = ps[j];
-			for (int k = now + next, cnt = 2; k <= 300000; k += next, cnt++)
-				dp[k] = min(dp[k], cnt);
-		}
-	}
-	cin >> n;
-	for (int i = 1; i <= n - 1; i++) {
-		int left = dp[n - i];
-		int right = dp[i];
-		if (left == 1e9 || right == 1e9) continue;
-		else dp[n] = min(dp[n], left + right);
-	}
-	cout << dp[n];
+	sort(vect.begin(), vect.end());
+	for (string str : vect)
+		cout << str << '\n';
 	return 0;
 }
