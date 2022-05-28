bb461c361fcac0ce51b2377b3578b4b80cde62a6
liardanc3 <liardanc3@gmail.com>
2022-05-29 01:18:39 +0900

    boj

--- tmp.cpp
+++ tmp.cpp
@@ -1,25 +1,22 @@
-﻿#include <iostream>
-
+﻿#include <bits/stdc++.h>
 using namespace std;
-
-void main() {
-
-    double e_value = 2.7182818284;
-    double X = 20037508.34;
-
-    double lat3857, long3857;
-
-    cin >> lat3857 >> long3857;
-    //converting the longitute from epsg 3857 to 4326
-    double long4326 = (long3857 * 180) / X;
-
-    //converting the latitude from epsg 3857 to 4326 split in multiple lines for readability        
-    double lat4326 = lat3857 / (X / 180);
-    double exponent = (3.141592 / 180) * lat4326;
-
-    lat4326 = atan(pow(e_value, exponent));
-    lat4326 = lat4326 / (3.141592 / 360); // Here is the fixed line
-    lat4326 = lat4326 - 90;
-    cout << lat4326 << ' ' << long4326;
-    exit(0);
+typedef pair<int, int> pii;
+vector<int> vect;
+vector<pii> _list;
+int n, sum;
+int main() {
+    for (int i = 1; i <= 8; i++) {
+        cin >> n;
+        _list.push_back({ n,i });
+    }
+    sort(_list.rbegin(), _list.rend());
+    for (int i = 0; i < 5; i++) {
+        sum += _list[i].first;
+        vect.push_back(_list[i].second);
+    }
+    sort(vect.begin(), vect.end());
+    cout << sum << '\n';
+    for (int i = 0; i < 5; i++)
+        cout << vect[i] << ' ';
+    return 0;
 }
