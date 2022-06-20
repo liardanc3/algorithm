bf303f04067b2130b448b8c10c5b9e2357d61647
liardanc3 <liardanc3@gmail.com>
2022-06-21 02:23:56 +0900

    boj

--- tmp.cpp
+++ tmp.cpp
@@ -1,20 +1,26 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
-int t, tmp;
-vector<int> vect;
+typedef tuple<int, int, int, string> student;
+int n, a, b, c;
+string name;
+vector<student> vect;
+bool cmp(student& a, student& b) {
+	if (get<0>(a) != get<0>(b))
+		return get<0>(a) > get<0>(b);
+	if (get<1>(a) != get<1>(b))
+		return get<1>(a) < get<1>(b);
+	if (get<2>(a) != get<2>(b))
+		return get<2>(a) > get<2>(b);
+	return get<3>(a) < get<3>(b);
+}
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    cin >> t;
-    while (t--) {
-        for (int i = 0; i < 10; i++) {
-            cin >> tmp;
-            vect.push_back(tmp);
-        }
-        sort(vect.begin(), vect.end());
-
-        cout << vect[7] << '\n';
-        vect.clear();
-    }
-    return 0;
+	cin >> n;
+	while (n--) {
+		cin >> name >> a >> b >> c;
+		vect.push_back({ a,b,c,name });
+	}
+	sort(vect.begin(), vect.end(), cmp);
+	for (student s : vect)
+		cout << get<3>(s) << '\n';
+	return 0;
 }
