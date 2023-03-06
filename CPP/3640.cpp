36593ee2d195c9f7ad88efc7984f710ae7fc17cc
liardanc3 <liardanc3@gmail.com>
2023-03-06 15:57:36 +0900

    Min cost Max flow

--- CPP/3640.cpp
+++ CPP/3640.cpp
@@ -1,30 +1,38 @@
 #include <bits/stdc++.h>
-#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 using namespace std;
+
+typedef pair<int, int> pii;
 
 struct node {
 	int next, capacity, pay, revidx;
 	node(int n, int c, int p, int r) {
 		next = n, capacity = c, pay = p, revidx = r;
 	}
-}; vector<node> vect[2001];
+}; vector<node> vect[2002];
 
-int t, v, e, a, b, c, visited[2001], idx[2001], dist[2001], inq[2001];
+int t, v, e, a, b, c, dist[2002], inq[2002], mx, fl;
+pii rev[2002];
 int spfa() {
-	fill(dist, dist + 2001, 1e9);
+	memset(rev, 0, sizeof rev);
+	fill(dist, dist + 2002, 1e9);
+	memset(inq, 0, sizeof inq);
 	inq[2] = 1; dist[2] = 0;
-	queue<int> q; q.push(1);
+	queue<int> q; q.push(2);
 	while (!q.empty()) {
 		int now = q.front(); q.pop();
 		inq[now] = 0;
 
+		int tick = -1;
 		for (node n : vect[now]) {
+			tick++;
 			int next = n.next;
 			int capacity = n.capacity;
 			int pay = n.pay;
+			int revidx = n.revidx;
 
 			if (dist[now] + pay < dist[next] && capacity > 0) {
 				dist[next] = dist[now] + pay;
+				rev[next] = { now, tick };
 				if (!inq[next]) {
 					inq[next] = 1;
 					q.push(next);
@@ -32,30 +40,57 @@ int spfa() {
 			}
 		}
 	}
-	return dist[2 * v] == 1e9 ? 0 : 1;
+	return dist[2 * v + 1] == 1e9 ? 0 : 1;
+}
+void dfs(int next, int wat) {
+	if (next == 2)
+		return;
+
+	int now = rev[next].first;
+	int tick = rev[next].second;
+
+	vect[now][tick].capacity -= 1;
+	vect[next][vect[now][tick].revidx].capacity += 1;
+	
+	dfs(now, min(wat,vect[now][tick].capacity));
 }
 signed main() {
-	fastio;
-	while (cin >> v) {
+	ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+	while (cin >> v >> e) {
 		if (cin.eof())
-			break;
-		for (int i = 1; i <= v; i++)
-			vect[i].clear();
-		cin >> e;
-		while (e--) {
-			cin >> a >> b >> c;
+			exit(0);
 
-			int in = 2 * a, out = 2 * a + 1, nxt = 2 * b;
+		[&]() {
+			for (int i = 1; i <= 2*v+1; i++)
+				vect[i].clear();
+			mx = 0;
+		}();
 
-			// in -> out;
-			vect[in].push_back({ out, a == 1 || a == 2 ? 1'000'000'000 : 1,0,(int)vect[b].size() });
-			vect[out].push_back({ in, 0, 0, (int)vect[out].size() - 1 });
 
-			// out -> nxt;
+		[&]() {
+			while (e--) {
+				cin >> a >> b >> c;
 
-		}
+				int out = 2 * a + 1, nxt = 2 * b;
 
+				// out -> nxt;
+				vect[out].push_back({ nxt, 1, c, (int)vect[nxt].size() });
+				vect[nxt].push_back({ out,0, -c,(int)vect[out].size() - 1 });
+			}
+			for (int i = 1; i <= v; i++) {
+				int in = i * 2, out = i * 2 + 1;
+
+				// in -> out
+				vect[in].push_back({ out, (i == 1 || i == v) ? 2 : 1,0,(int)vect[out].size() });
+				vect[out].push_back({ in, 0, 0, (int)vect[in].size() - 1 });
+			}
+		}();
+
+		spfa();
+		mx += dist[2 * v + 1];
+		dfs(2 * v + 1, 1e9);
+		spfa();
+		cout << mx + dist[2 * v + 1] << '\n';
 	}
-
 	return 0;
 }
