0e533556ea17aff951676d37afdf3bc86092be76
liardanc3 <liardanc3@gmail.com>
2023-03-14 21:20:23 +0900

    Max flow Min cut

--- CPP/12922.cpp
+++ CPP/12922.cpp
@@ -2,70 +2,104 @@
 using namespace std;
 
 typedef pair<int, int> pii;
-vector<pii> src, dst;
+vector<pii> dst;
 
 struct node {
 	int next, capacity, revidx;
 	node(int n, int c, int r) {
 		next = n, capacity = c, revidx = r;
 	}
-}; vector<node> vect[2502];
+}; vector<node> vect[5003];
 
-int N, arr[52][52], visited[2502], idx[2502], level[2502], mx;
+int N, arr[52][52], visited[5003], idx[5003], level[5003], mx;
 int dy[]{ -1,0,0,1 }, dx[]{ 0,-1,1,0 };
 string tmp;
-void addedge(int s, int d, int undirect) {
-	vect[s].push_back({ d,s == 0 ? 1'000'000'000 : 1,(int)vect[d].size() });
+void addedge(int s, int d, int c) {
+	vect[s].push_back({ d,c,(int)vect[d].size() });
 	vect[d].push_back({ s,0,(int)vect[s].size() - 1 });
-
-	if (undirect) 
-		addedge(d, s, 0);
 }
-void dfs(int y, int x, int direct, int dist) {
-	int start = (y - 1) * N + x;
-	if (y == dst[0].first && x == dst[0].second && direct == 0) {
-		addedge(start, 2501, 0);
-		return;
-	}
-	visited[start] = 1;
 
-	// ¹Ù´Ú¸é 1x1
-	if (!direct) {
-		for(int i=0; i<4; i++){
+void setnode() {
+	queue<pii> q;
+	q.push(dst[0]);
+	visited[2 * ((dst[0].first - 1) * N + dst[0].second)] = 1;
+
+	while (!q.empty()) {
+		pii p = q.front(); q.pop();
+		int y = p.first, x = p.second;
+		int start = 2 * ((y - 1) * N + x);
+
+		if (arr[y][x] == 2) {
+			addedge(start + 1, 0, 1e9);
+			continue;
+		}
+
+
+		for (int i = 0; i < 4; i++) {
 			int ddy1 = y + dy[i];
 			int ddy2 = y + 2 * dy[i];
+			int ddy3 = y + 3 * dy[i];
 			int ddx1 = x + dx[i];
 			int ddx2 = x + 2 * dx[i];
+			int ddx3 = x + 3 * dx[i];
 
-			int end1 = (ddy1 - 1) * N + ddx1;
-			int end2 = (ddy2 - 1) * N + ddx2;
+			int end1 = (ddy1 - 1) * N + ddx1; end1 += end1;
+			int end2 = (ddy2 - 1) * N + ddx2; end2 += end2;
+			int end3 = (ddy3 - 1) * N + ddx3; end3 += end3;
+
 			if (1 <= ddy1 && ddy1 <= N && 1 <= ddx1 && ddx1 <= N) {
-				if (arr[ddy1][ddx1] || arr[ddy2][ddx2]) {
+				if (arr[ddy1][ddx1] > 0) {
+					if (arr[ddy1][ddx1] == 1)
+						addedge(start + 1, end1, 1e9);
+					if (arr[ddy1][ddx1] == 2)
+						addedge(start + 1, end1, 1e9);
+					if (arr[ddy1][ddx1] == 3)
+						addedge(start + 1, end1, 1e9);
+				}
 
-					if (arr[ddy1][ddx1]) 
-						addedge(start, end1, 1);
-					if (arr[ddy2][ddx2]) 
-						addedge(start, end2, 1);
-					
-					if (!visited[end1])
-						dfs(ddy1, ddx1, i + 1, 2);
-					if (!visited[end2])
-						dfs(ddy2, ddx2, i + 1, 1);
+
+			}
+			if (1 <= ddy2 && ddy2 <= N && 1 <= ddx2 && ddx2 <= N) {
+				if (arr[ddy2][ddx2] > 0) {
+					if (arr[ddy2][ddx2] == 1)
+						addedge(start + 1, end2, 1e9);
+					if (arr[ddy2][ddx2] == 2)
+						addedge(start + 1, end2, 1e9);
+					if (arr[ddy2][ddx2] == 3)
+						addedge(start + 1, end2, 1e9);
 				}
 			}
-		}
-	}
+			if (1 <= ddy3 && ddy3 <= N && 1 <= ddx3 && ddx3 <= N) {
+				if (arr[ddy3][ddx3] == 0)
+					continue;
 
-	// ¹Ù´Ú¸é 2x1
-	else {
-		int ddy = y + dist * dy[direct - 1];
-		int ddx = x + dist * dx[direct - 1];
+				if (!visited[end3] && (arr[ddy1][ddx1] > 0 || arr[ddy2][ddx2] > 0)) {
+					visited[end3] = 1;
+					q.push({ ddy3,ddx3 });
+				}
 
-		int end = (ddy - 1) * N + ddx;
-		if (1 <= ddy && ddy <= N && 1 <= ddx && ddx <= N && arr[ddy][ddx]) {
-			addedge(start, end, 1);
-			if (!visited[end])
-				dfs(ddy, ddx, 0, 1);
+				if (arr[ddy3][ddx3] == 1) {
+					if (arr[ddy1][ddx1] > 0)
+						addedge(end1 + 1, end3, 1e9);
+					if (arr[ddy2][ddx2] > 0)
+						addedge(end2 + 1, end3, 1e9);
+				}
+
+				if (arr[ddy3][ddx3] == 2) {
+					if (arr[ddy1][ddx1] > 0)
+						addedge(end1 + 1, end3, 1e9);
+					if (arr[ddy2][ddx2] > 0)
+						addedge(end2 + 1, end3, 1e9);
+				}
+
+				if (arr[ddy3][ddx3] == 3) {
+					if (arr[ddy1][ddx1] > 0)
+						addedge(end1 + 1, end3, 1e9);
+					if (arr[ddy2][ddx2] > 0)
+						addedge(end2 + 1, end3, 1e9);
+				}
+			}
+
 		}
 	}
 
@@ -73,8 +107,8 @@ void dfs(int y, int x, int direct, int dist) {
 }
 int bfs() {
 	memset(level, -1, sizeof level);
-	level[0] = 1;
-	queue<int> q; q.push(0);
+	level[2 * ((dst[0].first - 1) * N + dst[0].second)] = 1;
+	queue<int> q; q.push(2 * ((dst[0].first - 1) * N + dst[0].second));
 	while (!q.empty()) {
 		int now = q.front(); q.pop();
 
@@ -89,10 +123,10 @@ int bfs() {
 		}
 	}
 
-	return level[2501] == -1 ? 0 : 1;
+	return level[0] == -1 ? 0 : 1;
 }
 int fl(int now, int wat) {
-	if (now == 2501)
+	if (now == 0)
 		return wat;
 
 	for (int& i = idx[now]; i < vect[now].size(); i++) {
@@ -113,36 +147,43 @@ int fl(int now, int wat) {
 	return 0;
 }
 int main() {
-	cin >> N;
-	for (int i = 1; i <= N; i++) {
-		cin >> tmp;
-		for (int j = 1; j <= N; j++) {
-			if (tmp[j-1] == '.')
-				arr[i][j] = 1;
-			else if (tmp[j-1] == 'b') {
-				addedge(0, (i - 1) * N + j, 0);
-				arr[i][j] = 1;
-				src.push_back({ i,j });
-			}
-			else if (tmp[j-1] == '$') {
-				arr[i][j] = 3;
-				dst.push_back({ i,j });
+	[&]() {
+		cin >> N;
+		for (int i = 1; i <= N; i++) {
+			cin >> tmp;
+			for (int j = 1; j <= N; j++) {
+				int now = (i - 1) * N + j;
+				if (tmp[j - 1] == '.') {
+					arr[i][j] = 1;
+					addedge(2 * now, 2 * now + 1, 1);
+				}
+				else if (tmp[j - 1] == 'b') {
+					arr[i][j] = 2;
+					addedge(2 * now, 2 * now + 1, 1e7);
+				}
+				else if (tmp[j - 1] == '$') {
+					arr[i][j] = 3;
+					addedge(2 * now, 2 * now + 1, 1e7);
+					dst.push_back({ i,j });
+				}
 			}
 		}
-	}
-	for (pii p : src) {
-		memset(visited, 0, sizeof(visited));
-		dfs(p.first, p.second, 0, 1);
-	}
+	}();
+
+	setnode();
+
 	while (bfs()) {
 		memset(idx, 0, sizeof(idx));
 		while (1) {
-			int flow = fl(0, 1e9);
+			int flow = fl(2 * ((dst[0].first - 1) * N + dst[0].second), 1e7);
 			if (!flow)
 				break;
 			mx += flow;;
 		}
 	}
-	cout << mx;
+	if (mx >= 1e7)
+		cout << -1;
+	else
+		cout << mx;
 	return 0;
 }
