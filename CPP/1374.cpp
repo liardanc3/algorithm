166e14d0e9c11d1b2cd690bd8eacfc581e17f5b9
liardanc3 <liardanc3@gmail.com>
2023-04-18 03:41:30 +0900

    bruteforce

--- CPP/1374.cpp
+++ CPP/1374.cpp
@@ -1,3 +1,29 @@
 #include <bits/stdc++.h>
-#define s std
-typedef s::pair<int, int>T;s::priority_queue<T,s::vector<T>,s::greater<T>>q;int N,a,b,x,r;int main(){s::cin>>N;while(N--){s::cin>>a>>a>>b;q.push({a,b});}while(!q.empty()){T k=q.top();q.pop();if(k.second){r++;q.push({k.second,0});}else r--;x=s::max(x,r);}s::cout<<x;return 0;}
+using namespace std;
+
+typedef pair<int, int> pii;
+priority_queue<pii, vector<pii>, greater<pii>> pq;
+
+int N, a, b, mx, room;
+int main() {
+	cin >> N;
+	while (N--) {
+		cin >> a >> a >> b;
+		pq.push({ a,b });
+	}
+	while (!pq.empty()) {
+		pii now = pq.top(); pq.pop();
+
+		if (now.second != -1) {
+			room++;
+			pq.push({ now.second, -1 });
+		}
+		else
+			room--;
+
+		mx = max(mx, room);
+	}
+
+	cout << mx;
+	return 0;
+}
