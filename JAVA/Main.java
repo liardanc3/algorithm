00b21cd71cb25684056086aa1e09b3add32101d0
kdh <liardanc3@gmail.com>
2024-04-04 21:14:35 +0900

    BOJ - hashMap

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -1,33 +1,14 @@
-import java.io.*;
-import java.sql.SQLOutput;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static Stack<Integer> st = new Stack<>();
 
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
 
-        int K = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<K; i++){
-            int input = Integer.parseInt(br.readLine());
-
-            if(input == 0){
-                st.pop();
-            } else {
-                st.push(input);
-            }
-        }
-
-        int answer = 0;
-        while(!st.empty()){
-            answer += st.pop();
-        }
-
-        System.out.println(answer);
     }
 }
