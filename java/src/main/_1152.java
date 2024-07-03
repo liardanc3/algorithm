40809e70d1d352e4131fd6d788ef0e9a93f25188
kdh <liardanc3@gmail.com>
2024-07-03 22:26:18 +0900

    BOJ

---
+++ java/src/main/_1152.java
@@ -0,0 +1,25 @@
+package src.main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1152 {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+    public static int cnt;
+
+    public static void main(String[] args) throws IOException {
+        String[] input = br.readLine().split(" ");
+
+        for (String s : input) {
+            if(!s.equals("")){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
+
