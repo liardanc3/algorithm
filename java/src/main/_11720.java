36b11723edec4e00d4734c4a45e2f87d2e1f1fd7
kdh <liardanc3@gmail.com>
2024-07-04 22:28:26 +0900

    BOJ

---
+++ java/src/main/_11720.java
@@ -0,0 +1,25 @@
+package src.main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11720 {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+
+    public static int N;
+    public static int answer;
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        String input = br.readLine();
+        for(int i=0; i<input.length(); i++){
+            answer += Integer.parseInt(String.valueOf(input.charAt(i)));
+        }
+
+        System.out.println(answer);
+    }
+}
+
