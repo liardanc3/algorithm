5e5d8012a7cc4016b6be6b4c651bb9ced00cc9c2
rami-kdh <liardanc3@gmail.com>
2025-07-16 17:01:25 +0900

    boj

--- java/src/main/_2480.java
+++ java/src/main/_2480.java
@@ -3,12 +3,12 @@ package src.main;
 import java.io.*;
 import java.util.Arrays;
 
-public class Main {
+public class _2480 {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static StringBuilder sb = new StringBuilder();
     public static int[] arr = new int[] {0,0,0,0,0,0,0};
+    public static int mx = 0;
 
     public static void main(String[] args) throws IOException {
         Arrays.stream(br.readLine().split(" "))
@@ -16,23 +16,15 @@ public class Main {
                 .forEach(num -> arr[num]++);
 
         for (int i = 1; i <= 6; i++) {
-            if (arr[i] != 0) {
-                sb = new StringBuilder();
-            }
-
             if (arr[i] == 3) {
-                sb.append(10_000 + i * 1_000);
-                System.out.println(sb.toString());
-                return;
+                mx = Math.max(mx, 10_000 + i * 1_000);
             } else if (arr[i] == 2) {
-                sb.append(1_000 + i * 100);
-                System.out.println(sb.toString());
-                return;
-            } else{
-                sb.append(i * 100);
+                mx = Math.max(mx, 1_000 + i * 100);
+            } else if (arr[i] == 1) {
+                mx = Math.max(mx, i * 100);
             }
         }
 
-        System.out.println(sb.toString());
+        System.out.print(mx);
     }
 }
