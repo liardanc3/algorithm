696fc9aab31396daa14cfaaac6be5a26ea621585
rami-kdh <liardanc3@gmail.com>
2025-07-21 15:45:46 +0900

    boj

---
+++ java/src/main/_10807.java
@@ -0,0 +1,24 @@
+package src.main;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class _10807 {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static StringBuilder sb = new StringBuilder();
+    public static int n, v;
+    public static int[] arr = new int[201];
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+
+        while (--n >= 0) {
+            arr[Integer.parseInt(input[n]) + 100]++;
+        }
+
+        System.out.println(arr[Integer.parseInt(br.readLine()) + 100]);
+    }
+}
