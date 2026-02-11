d8abaf753c80533a44f9d35d62a9cfd5c576f451
kdh <liardanc3@gmail.com>
2026-02-12 06:53:22 +0900

    BOJ

--- java/src/main/Main.java
+++ java/src/main/Main.java
@@ -1,38 +1,64 @@
 package main;
 
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static StringBuilder sb = new StringBuilder();
-    public static String inputA, inputB, inputC;
-    public static int a, b, c, d;
+    public static String input;
+    public static int n, m, nIdx = -1, mIdx = -1, len;
+    public static Map<Integer, Queue<Integer>> nNumber2IdxMap = new HashMap<>();
+    public static Map<Integer, Queue<Integer>> mNumber2IdxMap = new HashMap<>();
+    public static int[] arrN = new int[100];
+    public static int[] arrM = new int[100];
+    public static int[] answer = new int[100];
 
     public static void main(String[] args) throws IOException {
-        inputA = br.readLine();
-        inputB = br.readLine();
-        inputC = br.readLine();
-
-        if (!inputA.contains("zz")) {
-            d = Integer.parseInt(inputA) + 3;
-        } else if (!inputB.contains("zz")) {
-            d = Integer.parseInt(inputB) + 2;
-        } else if (!inputC.contains("zz")) {
-            d = Integer.parseInt(inputC) + 1;
-        } else {
-            assert (true);
+        n = Integer.parseInt(br.readLine());
+        arrN = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int i = 0; i < n; i++) {
+            Queue<Integer> idx = nNumber2IdxMap.getOrDefault(arrN[i], new ArrayDeque<>());
+            idx.add(i);
+            nNumber2IdxMap.put(arrN[i], idx);
         }
 
-        if (d % 3 == 0 && d % 5 == 0) {
-            System.out.println("FizzBuzz");
-        } else if (d % 3 == 0 && d % 5 != 0) {
-            System.out.println("Fizz");
-        } else if (d % 3 != 0 && d % 5 == 0) {
-            System.out.println("Buzz");
-        } else {
-            System.out.println(d);
+        m = Integer.parseInt(br.readLine());
+        arrM = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int i = 0; i < m; i++) {
+            Queue<Integer> idx = mNumber2IdxMap.getOrDefault(arrM[i], new ArrayDeque<>());
+            idx.add(i);
+            mNumber2IdxMap.put(arrM[i], idx);
         }
+
+        for (int i = 100; i >= 1; i--) {
+            if (nNumber2IdxMap.containsKey(i) && mNumber2IdxMap.containsKey(i)) {
+                Queue<Integer> nIdxQ = nNumber2IdxMap.get(i);
+                Queue<Integer> mIdxQ = mNumber2IdxMap.get(i);
+
+                while (!nIdxQ.isEmpty()) {
+                    Integer nIdxNow = nIdxQ.poll();
+
+                    while (nIdxNow > nIdx && !mIdxQ.isEmpty()) {
+                        Integer mIdxNow = mIdxQ.poll();
+
+                        if (mIdxNow > mIdx) {
+                            answer[len++] = i;
+                            mIdx = mIdxNow;
+                            nIdx = nIdxNow;
+                        }
+                    }
+                }
+            }
+        }
+
+        bw.write(len + "\n");
+        for (int i = 0; i < len; i++) {
+            bw.write(answer[i] + " ");
+        }
+        bw.flush();
+        bw.close();
     }
 }
