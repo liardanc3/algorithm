115d1462f706e2de1d91195a2ead4f6a7167db3a
kdh <liardanc3@gmail.com>
2026-03-30 04:14:35 +0900

    dp

--- java/src/main/_34547.java
+++ java/src/main/_34547.java
@@ -3,7 +3,7 @@ package main;
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class _34547 {
 
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
